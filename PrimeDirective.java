ae4be7d6fed6839dad201e07839a3e4c46a10547
clogerie <66565287+clogerie@users.noreply.github.com>
2022-10-25 11:25:21 -0400

    wrote logic to determine if number is prime

--- PrimeDirective.java
+++ PrimeDirective.java
@@ -1,7 +1,28 @@
+// import statement to use ArrayList
 import java.util.ArrayList;
-
+// PrimeDirective class
 class PrimeDirective {
+    // method to check if number is prime
+    public boolean isPrime(int number) {
+        // if number is 2 return true
+        if(number == 2) {
+            return true;\
+          // if number is less than 2 return false  
+        } else if(number < 2) {
+            return false;
+        }
+        // loop through numbers between 2 and given number
+        for(int i = 2; i < number; i++) {
+            // if number is divisible by i return false
+            if(number % i == 0) {
+                return false;
+            }
+        }
+        // otherwise return true
+        return true;
+    }
+    // main method
     public static void main(String[] args) {
-
+        
     }
 }
